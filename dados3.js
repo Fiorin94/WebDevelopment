de0c78724e8ff6c9fa3184158f3b644c08114566
LordFiorows <felipefiorin94@gmail.com>
2021-08-16 00:38:14 -0300

    Novos dados3.js

---
+++ dados3.js
@@ -0,0 +1,15 @@
+let idade = 31;
+
+
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4756.50;
+
+console.log(typeof salario);
+
+//Javascript não faz diferenciação de tipos de números, tudo é number, mesmo int ou float.
+
+let estaChovendo = true //ou false
+
+console.log(typeof estaChovendo);
