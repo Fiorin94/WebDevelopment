e8c97a3aa88649ffca2aaffc9091a11e2a51ae11
LordFiorows <felipefiorin94@gmail.com>
2021-08-31 19:00:19 -0300

    Aula de loop com var e let

---
+++ Fundamentos/letEmLoop.js
@@ -0,0 +1,7 @@
+for(let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log("i= ", i)
+
+//No let o valor da variável não funciona fora do loop
---
+++ Fundamentos/varEmLoop.js
@@ -0,0 +1,7 @@
+for (var i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log('i = ', i)
+
+//Com a variável var é possível utilizá-la fora do loop
